020d8fb5da0c9cefdba5c320a2171f273b2db091
passi <pascalfeyer@gmail.com>
2021-04-12 23:01:01 +0200

    +- types, +- introduction

--- README.md
+++ README.md
@@ -1,167 +1,255 @@
 # elm-typed-value
 
-> Create type-safe aliases for primitive types
+> no more 1-constructor types
 
 Fundamental concepts are similar to [Prior art](#Prior-Art):
 
-A value is wrapped in a `type` with a phantom `tag`, so that a `Typed A Int ...` is not a `Typed B Int ...`.
+A value is wrapped in the `type Typed` with a phantom `tag`.
+So in the end, a `Typed Dollar Int ...` can't be called a `Typed Euro Int ...`.
 
-There are 3 wrappers in this package 
+For **every** `type` that has just 1 constructor with a value
+```elm
+type Special
+    = Special Value
+```
+some `Typed` is better suited!
 
-- `ReadOnly`:
-    - only its value can be read everywhere
-    - creating new ones & updating is only possible inside the module
-- `WriteOnly`:
-    - can be updated everywhere
-    - its value can never be read
-- `ReadWrite`:
-    - you can both access the value & create new ones
+You get rid of always writing the same mehods for those types
 
-→ additional type-safety.
+```elm
+extract (Special value) =
+    value
 
-## examples
+map alter (Special value) =
+    Special (alter value)
+
+--...
+```
+
+while adding type-safety on your way.
+
+There are 2 kinds of `Typed`:
+
+  - `Checked`, if the type should only contain "validated" values
+
+    ```elm
+    module NaturalNumber exposing (NaturalNumber)
+
+    -- nobody can create a NaturalNumber outside this module
+    type NaturalNumber =
+        NaturalNumber Int
+    ```
+
+    Creating & updating `NaturalNumber`s will only be possible inside that module.
+
+  - `Tagged`, if you want to write
+
+    ```elm
+    -- constructor can be used anywhere
+    type Meters =
+        Meters Float
+    ```
+
+    Users can then access the `Float` `value`, update it & create new `Meters` everywhere
+
+
+If you don't want users to access the `value` directly, use `CheckedHidden` / `TaggedHidden`.
+
+
+# examples
 
 ```elm
 import Typed exposing
-    (Typed, WriteOnly, ReadOnly, writeOnly)
+    ( Tagged, Checked, CheckedHidden, TaggedHidden
+    , tag, hideValue, hiddenValueIn, isChecked
+    )
 ```
 
-### `ReadWrite`
-
-Basically a `type alias` with a phantom tag:
+## `Tagged`
 
 ```elm
 type alias Length unit =
-    ReadWrite unit Float
+    Tagged (LengthUnit unit) Float
 
-type Meters = Meters
-type Millimeters = Millimeters
+type LengthUnit unit
+    = LengthUnit Never
 
--- use a type annotation: this should only convert meters
+type Meters = Meters Never
+type Millimeters = Millimeters Never
+
+-- use a type annotation to say which units are translated
 metersToMillimeters : Length Meters -> Length Millimeters
 metersToMillimeters =
     Typed.map ((*) 1000)
-        >> Typed.is Millimeters
 
 -- annotate to set the unit
 heightEiffelTower : Length Meters
 heightEiffelTower =
-    Typed.new Meters 300
+    tag 300
 ```
 
-### `ReadOnly`
+## `Checked`
 
 ```elm
--- must be in a seperate module
-module DivisibleBy2 exposing
-    ( DivisibleBy2, multiply, add
+module Even exposing
+    ( Even
+    , multiply, add
     , zero, two
     )
 
-type alias DivisibleBy2 =
-    ReadOnly DivisibleBy2Tag Int
+type alias Even =
+    Checked EvenTag Int
 
 -- don't expose this
-type DivisibleBy2Tag =
-    DivisibleBy2
+type EvenTag = Even
 
-multiply : Int -> DivisibleBy2 -> DivisibleBy2
+multiply : Int -> Even -> Even
 multiply int =
     Typed.map ((*) int)
-        >> Typed.is DivisibleBy2
+        >> isChecked Even
 
-add : DivisibleBy2 -> DivisibleBy2 -> DivisibleBy2
+add : Even -> Even -> Even
 add toAdd =
     Typed.map2 (+) toAdd
-        >> Typed.is DivisibleBy2
+        >> isChecked Even
 
-zero : DivisibleBy2
+zero : Even
 zero =
-    Typed.new DivisibleBy2 0
+    tag 0 |> isChecked Even
 
-two : DivisibleBy2
+two : Even
 two =
-    Typed.new DivisibleBy2 2
+    tag 2 |> isChecked Even
 ```
 
 Then outside this module
 
 ```elm
-iWantANumberDivisibleBy2 : DivisibleBy2 -> Cake
+iWantANumberEven : Even -> Cake
 
-iWantANumberDivisibleBy2
-    (Typed.new 3 {-tag not available-})
+iWantANumberEven (tag 3)
+--> compile-time error: isn't of type Checked
 
-iWantANumberDivisibleBy2
-    (DivisibleBy2.two
-        |> DivisibleBy2.multiply -5
-    )
+iWantANumberEven
+    (Even.two |> Even.multiply -5)
 --> Cake
 ```
-Another example:
+
+## `CheckedHidden`
+
+A validated value that can't be accessed by a user.
 
 A module that only exposes randomly generated unique `Id`s:
+
 ```elm
-module Id exposing (Id, random)
+module Id exposing (Id, random, toBytes, toString)
 
 import Random
 
-type IdTag = Id
-
 type alias Id =
-    ReadOnly IdTag String
+    CheckedHidden IdTag CurrentImplementation
+
+-- left as an implementation detail
+-- might change in the future, but the API should stay the same
+type alias CurrentImplementation =
+    String
+
+type IdTag = Id
 
 random : Random.Generator Id
 random =
-    Random.map (Typed.new Id)
+    Random.map (isChecked Id)
         ({-...-})
+
+toBytes --...
+toString --...
 ```
 No `Id` can be created outside this package!
 
-### `WriteOnly`
-
-You should only need this rarely.
+### Combined with `TaggedHidden`
 
 ```elm
+module Password exposing (Password, GoodPassword, isGood)
+
 type alias Password =
-    WriteOnly PasswordTag String
+    TaggedHidden PasswordTag String
 
-type PasswordTag = Password
+type alias GoodPassword =
+    CheckedHidden GoodPasswordTag String
 
+-- don't expose any tag
+type PasswordTag = Password Never
+type GoodPasswordTag = GoodPassword
+
+isGood :
+    Password -> Result String GoodPassword
+isGood passwordToTest =
+    let
+        commonPasswords =
+            Set.fromList
+                [ "password1234", "secret1234"
+                , "c001_p4ssw0rd", "1234567890"
+                --...
+                ]
+        
+        passwordString =
+            hiddenValueIn Password passwordToTest
+    in
+    if (passwordToTest |> String.length) < 10 then
+        "Use at lest 10 letters & symbols."
+    else if Set.member passwordToTest commonPasswords then
+        "Choose a less common password."
+    else
+        Ok (passwordToTest |> isChecked GoodPassword)
+```
+You can then decide that only a part of the information should be accessible.
+```elm
+{-| Doesn't expose too much information.
+-}
+toOnlyDots : Password -> List Char
+toOnlyDots =
+    hiddenValueIn Password
+        >> String.length
+        >> (\dots-> List.repeat dots '·')
+```
+In another module
+```elm
 type alias User =
     { name : String
-    , password : Password
+    , password : GoodPassword
+        --there can't be a user with a insecure password
     }
+
+type Msg
+    --cannot access the password the user changed to
+    = ChangePassword Password
 
 update msg model =
     case msg of
-        ChangePassword password ->
+        ChangePassword newPassword ->
             { model
               | user =
                   { name = model.user.name
                   , password =
-                      Typed.new Password password --valid
+                      case Password.isGood newPassword of
+                          Ok goodPasword ->
+                              -- valid
+                          Err message ->
+                              -- tell the user
                   }
             }
 
+view model =
+    button [ onPress (tag >> ChangePassword) ]
+        [ text "Change Password" ]
+```
+→ There can't be a `User` with a bad password.
+```elm
+leak (Typed.value newPassword)
+-- or
 leak (Typed.value model.user.password)
 ```
-→ You can't get the value inside `password`. This is a compile-time error.
-
-However, there's one thing you can still do
-```elm
-commonPasswords =
-    Set.fromList
-        [ "password", "secret", "p4ssw0rd", "1234" ]
-        |> Set.map (Typed.new Password)
-
-if Set.member model.user.password commonPasswords then
-    "Choose a less common password. Use at lest 10 letters & symbols."
-else
-    "👍"
-```
-
-But if you wanted to find out if the length is >= 10 for example, just use `ReadOnly`.
+→ compile-time error: Can't get the `value` inside `password`.
 
 ## Prior art
 This package wouldn't exist without a lot of inspiration from those packages.
--- src/Typed.elm
+++ src/Typed.elm
@@ -1,45 +1,49 @@
 module Typed exposing
-    ( Typed, new
-    , ReadWrite
-    , Readable
-    , value, values2
-    , ReadOnly, is
-    , Writable
+    ( Typed, NoUser, Anyone
+    , tag
+    , Tagged
+    , Checked, isChecked
+    , TaggedHidden, CheckedHidden, hideValue
+    , value, values2, hiddenValueIn
     , map, map2
-    , WriteOnly, writeOnly
     )
 
 {-|
 
-@docs Typed, new
 
-Both `Readable` & `Writable`:
+## building blocks
 
-@docs ReadWrite
-
-
-## read
-
-@docs Readable
-
-@docs value, values2
+@docs Typed, NoUser, Anyone
 
 
-### read-only
+## create
 
-@docs ReadOnly, is
+@docs tag
 
 
-## write
+## no need to check
 
-@docs Writable
+@docs Tagged
+
+
+## checked
+
+@docs Checked, isChecked
+
+
+## hidden
+
+@docs TaggedHidden, CheckedHidden, hideValue
+
+
+## access
+
+@docs value, values2, hiddenValueIn
+
+
+## modify
 
 @docs map, map2
-
-
-### write-only
-
-@docs WriteOnly, writeOnly
 
 -}
 
@@ -47,91 +51,124 @@ Both `Readable` & `Writable`:
 {-| A value is wrapped in a `type` with a phantom `tag`,
 so that a `Typed A Int ...` is not a `Typed B Int ...`.
 
-There are 3 wrappers of type `Typed`
+The wrappers of type `Typed`
 
-  - [`ReadOnly`](Typed#ReadOnly):
-      - only its value can be read everywhere
-      - creating new ones & updating is only possible inside the module
-  - [`WriteOnly`](Typed#WriteOnly):
-      - can be updated everywhere
-      - its value can never be read
-  - [`ReadWrite`](Typed#ReadWrite):
-      - you can both access the value & create new ones
+  - [`Checked`](Typed#Checked)
 
-→ additional type-safety.
+  - [`Tagged`](Typed#Tagged)
 
-You will mostly see the type `Typed` as a function argument type.
+  - [`TaggedHidden`](Typed#TaggedHidden)
+
+  - [`CheckedHidden`](Typed#TaggedHidden)
+
+all promise additional type-safety.
+
+You will see `Typed` as a function argument type.
 
     map :
-        (value -> value)
-        -> Typed tag value { read | write : write }
-        -> Writable tag value read
+        (value -> mappedValue)
+        -> Typed tag value { whoCanAccess | canCreate : whoCanCreate }
+        -> Typed mappedTag mappedValue { whoCanAccess | canCreate : Anyone }
 
-Is saying: Works for every `Typed`.
-The result is `Writable` and has the same `read` permission.
+Is saying: `map` works on every `Typed`.
+The result is updatable & and has the same permission on `whoCanCreate`.
+
+Meaning if the input was
+
+  - `Checked` or `Tagged`, the result becomes a `Tagged`
+  - `CheckedHidden` or `TaggedHidden`, the result becomes a `TaggedHidden`
 
 -}
-type Typed tag value readWritePermissions
+type Typed tag value accessibilityAndCreatability
     = Typed value
 
 
-{-| Create a new tagged value that can be `ReadOnly`. If you start modifying that value, it becomes a `ReadWrite`.
+{-| Create a new tagged value.
+
+  - can be `Checked` with [`is`](Typed#is)
+  - can be `TaggedHidden` with [`hideValue`](Typed#hideValue)
+  - if you start modifying that value, it becomes a `Tagged`
+
 -}
-new : tag -> value -> Readable tag value write
-new _ value_ =
+tag : value -> Typed tag value { accessiblility | canCreate : Anyone }
+tag value_ =
     Typed value_
 
 
-type Allowed
-    = Allowed Never
-
-
-type NotAllowed
-    = NotAllowed Never
-
-
-{-| Allows accessing the value, creating & updating instances.
+{-| Anyone.
 -}
-type alias ReadWrite tag value =
-    Typed tag value { write : Allowed, read : Allowed }
+type Anyone
+    = Anyone Never
+
+
+{-| Only the ones with access to the `tag` constructor.
+-}
+type NoUser
+    = NoUser Never
+
+
+{-| Every possible `value` can be a `tag`.
+
+  - Tagged MetersTag Float
+      - **✓** Every Float can describe `Meters`
+  - Tagged NaturalNumberTag Int
+      - **⨯** Not every `Int` can be a `Nat`
+      - Use `Checked` instead!
+
+You can ccess its `value`.
+
+An instance can be created & updated everywhere.
+
+-}
+type alias Tagged tag value =
+    Typed
+        tag
+        value
+        { canAccess : Anyone
+        , canCreate : Anyone
+        }
 
 
 
 -- ## read
 
 
-{-| Accessing its value is allowed.
+{-| Instances that can only be validated in the module where the `tag` is.
+
+You can read the valid `value`, but once you modify it, it just becomes a `Tagged`.
+
 -}
-type alias Readable tag value write =
-    Typed tag value { write | read : Allowed }
+type alias Checked tag value =
+    Typed
+        tag
+        value
+        { canAccess : Anyone
+        , canCreate : NoUser
+        }
 
 
-{-| Creating or updating instances isn't allowed.
+{-| Read the value inside the `Accessible`.
 -}
-type alias ReadOnly tag value =
-    Typed tag value { read : Allowed, write : NotAllowed }
-
-
-{-| Read the value inside the `Readable`.
--}
-value : Readable tag value write -> value
+value :
+    Typed tag value { whoCanCreate | canAccess : Anyone }
+    -> value
 value =
     \(Typed value_) -> value_
 
 
-{-| Use the values of 2 `Readable`s to return a result.
+{-| Use the values of 2 `Accessible`s to return a result.
 
     type alias PrimeNumber =
-        ReadOnly PrimeNumberTag Int
+        Checked PrimeNumberTag Int
 
     prime3 : PrimeNumber
     prime3 =
-        Typed.new PrimeNumber 3
+        tag PrimeNumber 3
 
     prime5 =
-        Typed.new PrimeNumber 5
+        tag PrimeNumber 5
 
-Everywhere
+Anywhere
 
     Typed.values2 (+) prime3 prime5
     --> 8
@@ -142,21 +179,21 @@ Everywhere
 -}
 values2 :
     (aValue -> bValue -> resultValue)
-    -> Readable aTag aValue aWrite
-    -> Readable bTag bValue bWrite
+    -> Typed aTag aValue { aCreatability | canAccess : Anyone }
+    -> Typed bTag bValue { bCreatability | canAccess : Anyone }
     -> resultValue
 values2 binOp aTyped bTyped =
     binOp (value aTyped) (value bTyped)
 
 
-{-| After you modified a `ReadOnly`, it becomes a `ReadWrite`. To transform it back, use `is tag`.
+{-| After called `tag` or modified a checked value, you get a `Tagged`. To transform it back, use `isChecked tag`.
 
-The `tag` verifies that you are still allowed to create `ReadOnly`s with that `tag`.
+The `tag` verifies that you are allowed to say a `value` is `Checked`s with that `tag`.
 
 The type of tag might even change in that operation.
 
     type alias Length unit =
-        ReadWrite unit Float
+        Tagged unit Float
 
     type Meters
         = Meters
@@ -168,31 +205,25 @@ The type of tag might even change in that operation.
     metersToMillimeters : Length Meters -> Length Millimeters
     metersToMillimeters =
         Typed.map ((*) 1000)
-            >> Typed.is Millimeters
+            >> isChecked Millimeters
 
 -}
-is :
-    readOnlyTag
-    -> Readable tag value write
-    -> Readable readOnlyTag value resultWrite
-is _ =
-    value >> Typed
+isChecked :
+    checkedTag
+    -> Typed tag value { whoCanAccess | canCreate : create }
+    -> Typed checkedTag value { whoCanAccess | canCreate : Anyone }
+isChecked _ =
+    \(Typed value_) -> Typed value_
 
 
 
--- ## write
-
-
-{-| Allow updating creating new instances or.
--}
-type alias Writable tag value read =
-    Typed tag value { read | write : Allowed }
+-- ## creatable by user
 
 
 {-| Using its value isn't allowed.
 
     type alias Password =
-        WriteOnly PasswordTag String
+        TaggedHidden PasswordTag String
 
     type PasswordTag
         = Password Never
@@ -207,16 +238,21 @@ type alias Writable tag value read =
         -- compile-time error
         user.password |> Typed.value
 
-The only thing you can still use is `==` on 2 `WriteOnly`s of the same type.
+The only thing you can still use is `==` on 2 `TaggedHidden`s of the same type.
 
 -}
-type alias WriteOnly tag value =
-    Typed tag value { write : Allowed, read : NotAllowed }
+type alias TaggedHidden tag value =
+    Typed
+        tag
+        value
+        { canCreate : Anyone
+        , canAccess : NoUser
+        }
 
 
 {-| Alter the value inside.
 
-If the `Typed` was a `ReadOnly`, it becomes a `Typed`.
+If the `Typed` was a `Checked`, it becomes a `Typed`.
 
     type alias Meters =
         Typed MetersTag Int Typed
@@ -230,47 +266,47 @@ If the `Typed` was a `ReadOnly`, it becomes a `Typed`.
 
 -}
 map :
-    (value -> value)
-    -> Typed tag value { read | write : write }
-    -> Writable tag value read
+    (value -> mappedValue)
+    -> Typed tag value { whoCanAccess | canCreate : whoCanCreate }
+    -> Typed mappedTag mappedValue { whoCanAccess | canCreate : Anyone }
 map alter =
     \(Typed value_) -> alter value_ |> Typed
 
 
 {-| Use the values of 2 `Typed`s to return a result.
 
-The result becomes a `Writable` with the same reading permission as the 2 inputs.
+The result becomes a `CreatableAndModifiable` with the same reading permission as the 2 inputs.
 
     type alias PrimeNumber =
-        Typed PrimeNumberTag Int ReadOnly
+        Typed PrimeNumberTag Int Checked
 
     prime3 : PrimeNumber
     prime3 =
-        readOnly PrimeNumber (tag 3)
+        tag 3 |> isChecked PrimeNumber
 
     prime5 =
-        readOnly PrimeNumber (tag 5)
+        tag 5 |> isChecked PrimeNumber
 
-In another module
+Anywhere
 
-    Typed.values2 prime3 prime5
+    Typed.values2 (+) prime3 prime5
     --> 8
 
 In another module
 
     type alias NonPrime =
-        Typed NonPrimeTag Int ReadOnly
+        Checked NonPrimeTag Int
 
     fromMultiplyingPrimes aPrime bPrime =
         Typed.map2 (*) aPrime bPrime
-            |> readOnly NonPrime
+            |> isChecked NonPrime
 
 -}
 map2 :
-    (value -> value -> value)
-    -> Typed tag value { read | write : aWrite }
-    -> Typed tag value { read | write : bWrite }
-    -> Writable tag value read
+    (aValue -> bValue -> combinedValue)
+    -> Typed aTag aValue { whoCanAccess | canCreate : aWhoCanCreate }
+    -> Typed bTag bValue { whoCanAccess | canCreate : bWhoCanCreate }
+    -> Typed combinedTag combinedValue { whoCanAccess | canCreate : Anyone }
 map2 binOp aTyped bTyped =
     let
         (Typed aValue) =
@@ -282,8 +318,46 @@ map2 binOp aTyped bTyped =
     binOp aValue bValue |> Typed
 
 
-{-| Stop allowing access to the `value`.
+
+-- ## hidden
+
+
+{-| A `Checked` value, but not everything about the `value` is exposed. This allows
+
+  - hiding implementation details
+  - hiding data you don't want users to see
+
 -}
-writeOnly : Writable tag value read -> WriteOnly tag value
-writeOnly =
+type alias CheckedHidden tag value =
+    Typed
+        tag
+        value
+        { canCreate : NoUser
+        , canAccess : NoUser
+        }
+
+
+{-| Stop users to access to the `value`.
+
+  - A `Checked` becomes a `CheckedHidden`
+  - A `Tagged` becomes a `TaggedHidden`
+
+-}
+hideValue :
+    Typed tag value { whoCanCreate | canAccess : whoCanAccess }
+    -> Typed tag value { whoCanCreate | canAccess : NoUser }
+hideValue =
     \(Typed value_) -> Typed value_
+
+
+{-| If you have a `CheckedHidden`, its value isn't readable by users.
+
+If you have the `tag` however, you can access this data hidden from the user.
+
+-}
+hiddenValueIn :
+    tag
+    -> Typed tag value { whoCanCreate | canAccess : NoUser }
+    -> value
+hiddenValueIn _ =
+    \(Typed value_) -> value_
