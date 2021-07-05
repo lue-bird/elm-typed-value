b750e2962214b8d7f3bdbfe418709a6eb0286317
passi <pascalfeyer@gmail.com>
2021-07-05 21:06:16 +0200

    + review

---
+++ review/src/ReviewConfig.elm
@@ -0,0 +1,23 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import Review.Rule exposing (Rule)
+import NoUnused.Dependencies
+import OnlyAllSingleUseTypeVarsEndWith_
+
+
+config : List Rule
+config =
+    [ NoUnused.Dependencies.rule
+    , OnlyAllSingleUseTypeVarsEndWith_.rule
+    ]
