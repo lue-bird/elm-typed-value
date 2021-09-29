f39d94be464ac8d2f0052d302b9c934134f5b8ec
lue-bird <lue.the.bird@gmail.com>
2021-09-29 21:33:07 +0200

    + newlines

--- examples/src/UsePixels.elm
+++ examples/src/UsePixels.elm
@@ -3,11 +3,13 @@ module UsePixels exposing (defaultWindowWidth)
 import Pixels exposing (Pixels)
 import Typed exposing (tag)
 
+
 defaultWindowWidth : Pixels
 defaultWindowWidth =
     Typed.map2 (+)
         (tag 700)
         (borderWidth |> Typed.map ((*) 2))
+
 
 borderWidth : Pixels
 borderWidth =
